c4c5c67a256056dbe657482e66017e8bea68f41f
Josh <jlwarren1999@gmail.com>
2022-05-30 17:11:01 +0800

    Upload version 1.0.0 source code

---
+++ PATHAnything.sh
@@ -0,0 +1,20 @@
+# OSMANiC PATHAnything Copyright 2022
+# Version 1.0.0
+# This is free software, obtain a copy at https://github.com/OSMANiCTeam/PATHAnything
+# If you paid for this software, demand a refund
+
+echo IF YOU ARE TRYING TO ADD THIS SCRIPT TO PATH, CREATE A COPY OF IT, USE THE COPY TO ADD THIS SCRIPT TO PATH, THEN DELETE THE COPY. MOVING A RUNNING INSTANCE CAN CAUSE PROBLEMS AND ISSUES!!!
+echo 
+echo Type the file name of the bash script to want to add to PATH, including the file extension
+read file_name
+echo Type what you want to name the command
+read command_name
+echo Sudo permissions are needed to add executable permissions to your script and add it to PATH.
+sudo chmod +x $file_name
+sudo mv $file_name /usr/bin/$command_name
+echo 
+echo Command Directory:
+which $command_name
+echo 
+echo Script finished! If you see a file or directory error above, the script did not get added successfully
+echo You can remove your script at any time by typing "sudo rm $command_name"
